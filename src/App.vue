139664c79e48cb38b9267d7364ec41a9a930b007
stefanelli990 <stefan.mohenski@gmail.com>
2023-12-25 02:46:03 +0100

    move fetch logic to country list and refactor rendering countries

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,9 @@
 
 <script setup>
 
-import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
 import AppHeader from './components/AppHeader.vue'
 
-import { useCountriesStore} from './stores/countriesStore'
-
-const countriesStore = useCountriesStore()
-
-const fetchData = async() => {
-    fetch(`https://restcountries.com/v3.1/all`)
-    .then(response => response.json())
-    .then(data => {
-        countriesStore.countriesData = data.sort((a, b) => a.name.common.localeCompare(b.name.common))
-        console.log(countriesStore.countriesData)
-    })
-}
-
-onMounted(() => {
-    fetchData()
-})
 </script>
 
 
--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,40 +1,45 @@
 <template>
-
-    <ul class="grid grid-cols-3 gap-4" v-if="countriesStore.filter === 'All'">
-        <CountryItem v-for="(country, index) in countriesStore.filterCountries" :key="country.id || index" :name="country.name.common" :flag="country.flags.svg" :coat-of-arms="country.coatOfArms.svg" :population="country.population" :capital="country.capital && country.capital.length > 0 ? country.capital[0] : 'N/A'" :area="country.area" :code="country.cca2"/>
+    <div v-if="isLoading">Loading...</div>
+    <ul v-else-if="getFilteredCountries.length > 0" class="grid grid-cols-3 gap-4">
+        <CountryItem v-for="(country, index) in getFilteredCountries" :key="country.id || index" :name="country.name.common" :flag="country.flags.svg" :coat-of-arms="country.coatOfArms.svg" :population="country.population" :capital="country.capital && country.capital.length > 0 ? country.capital[0] : 'N/A'" :area="country.area" :code="country.cca2"/>
     </ul>
-
-    <ul class="grid grid-cols-3 gap-4" v-else-if="countriesStore.filter === 'Africa'">
-        <CountryItem v-for="(country, index) in countriesStore.filterByContinent('Africa')" :key="country.id || index" :name="country.name.common" :flag="country.flags.svg" :coat-of-arms="country.coatOfArms.svg" :population="country.population" :capital="country.capital && country.capital.length > 0 ? country.capital[0] : 'N/A'" :area="country.area" :code="country.cca2"/>
-    </ul>
-
-    <ul class="grid grid-cols-3 gap-4" v-else-if="countriesStore.filter === 'Asia'">
-        <CountryItem v-for="(country, index) in countriesStore.filterByContinent('Asia')" :key="country.id || index" :name="country.name.common" :flag="country.flags.svg" :coat-of-arms="country.coatOfArms.svg" :population="country.population" :capital="country.capital && country.capital.length > 0 ? country.capital[0] : 'N/A'" :area="country.area" :code="country.cca2"/>
-    </ul>
-
-    <ul class="grid grid-cols-3 gap-4" v-else-if="countriesStore.filter === 'Europe'">
-        <CountryItem v-for="(country, index) in countriesStore.filterByContinent('Europe')" :key="country.id || index" :name="country.name.common" :flag="country.flags.svg" :coat-of-arms="country.coatOfArms.svg" :population="country.population" :capital="country.capital && country.capital.length > 0 ? country.capital[0] : 'N/A'" :area="country.area" :code="country.cca2"/>
-    </ul>
-
-    <ul class="grid grid-cols-3 gap-4" v-else-if="countriesStore.filter === 'North America'">
-        <CountryItem v-for="(country, index) in countriesStore.filterByContinent('North America')" :key="country.id || index" :name="country.name.common" :flag="country.flags.svg" :coat-of-arms="country.coatOfArms.svg" :population="country.population" :capital="country.capital && country.capital.length > 0 ? country.capital[0] : 'N/A'" :area="country.area" :code="country.cca2"/>
-    </ul>
-
-    <ul class="grid grid-cols-3 gap-4" v-else-if="countriesStore.filter === 'South America'">
-        <CountryItem v-for="(country, index) in countriesStore.filterByContinent('South America')" :key="country.id || index" :name="country.name.common" :flag="country.flags.svg" :coat-of-arms="country.coatOfArms.svg" :population="country.population" :capital="country.capital && country.capital.length > 0 ? country.capital[0] : 'N/A'" :area="country.area" :code="country.cca2"/>
-    </ul>
-
-    <ul class="grid grid-cols-3 gap-4" v-else-if="countriesStore.filter === 'Oceania'">
-        <CountryItem v-for="(country, index) in countriesStore.filterByContinent('Oceania')" :key="country.id || index" :name="country.name.common" :flag="country.flags.svg" :coat-of-arms="country.coatOfArms.svg" :population="country.population" :capital="country.capital && country.capital.length > 0 ? country.capital[0] : 'N/A'" :area="country.area" :code="country.cca2"/>
-    </ul>
-
+    <div v-else>
+        No countries found for the searched term.
+    </div>
 </template>
 
 <script setup>
-
+import { ref, computed, onMounted } from 'vue'
 import { useCountriesStore } from '../stores/countriesStore'
 import CountryItem from './CountryItem.vue'
 
 const countriesStore = useCountriesStore()
+
+const isLoading = ref(true)
+
+const getFilteredCountries = computed(() => {
+  if (countriesStore.filter === 'All') {
+    return countriesStore.filterCountries
+  } else {
+    return countriesStore.filterByContinent(countriesStore.filter)
+  }
+})
+
+const fetchData = async () => {
+  isLoading.value = true;
+  try {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data = await response.json();
+    countriesStore.countriesData = data.sort((a, b) => a.name.common.localeCompare(b.name.common))
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  } finally {
+    isLoading.value = false
+  }
+}
+
+onMounted(() => {
+  fetchData()
+})
 
 </script>
