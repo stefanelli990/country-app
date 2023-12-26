90fe4387606249ba9569d28d2aa174e1493ccd56
stefanelli990 <stefan.mohenski@gmail.com>
2023-12-27 00:50:56 +0100

    styling back btn add fixed country details

--- src/components/CountryItem.vue
+++ src/components/CountryItem.vue
@@ -1,18 +1,15 @@
 <!-- CountryItem.vue -->
 
 <template>
-    <li class="bg-white rounded-md shadow-sm overflow-hidden cursor-pointer p-4">
-        <span :class="countryCodez" class="text-7xl border border-gray-100 rounded-md mb-2"></span>
-        <h1 class="text-lg font-bold mb-2">{{ name }}</h1>
-        <p class="mb-1"><span class="font-bold">Area:</span> {{ area.toLocaleString() }} km<sup>2</sup></p>
-        <p class="mb-1"><span class="font-bold">Population:</span> {{ population.toLocaleString() }}</p>
-        <p><span class="font-bold">Capital:</span> {{ capital }}</p>
+    <li class="bg-white flex flex-col items-center text-center space-y-2 rounded-md shadow-sm overflow-hidden cursor-pointer p-8">
+        <span :class="countryCodez" class="border border-gray-100 rounded-md mb-2 text-[125px]"></span>
+        <h1 class="text-lg font-bold">{{ name }}</h1>
     </li>
 </template>
 
 <script setup>
 import { computed } from 'vue'
-const props = defineProps(['name','flag','population','capital','area','code'])
+const props = defineProps(['name','flag','population','capital','area','code','cca3'])
 
 
 const countryCodez = computed(() => {
@@ -20,3 +17,4 @@ const countryCodez = computed(() => {
 })
 
 </script>
+
--- src/views/CountryDetailsView.vue
+++ src/views/CountryDetailsView.vue
@@ -2,39 +2,55 @@
 
 <template>
   <main class="max-w-3xl">
-    <!-- I added v-if="countryDetails" to check if countryDetails is not null or undefined before attempting to access countryDetails.name.common. This is a precautionary measure to avoid errors if countryDetails is initially null.
-
-    When you fetch data asynchronously, there is a brief moment when the data may not be available, and trying to access properties on null or undefined can lead to runtime errors. By using v-if, you ensure that the content inside the h2 tag is only rendered when countryDetails is truthy (i.e., not null or undefined).
-
-    If countryDetails is null, the <div> element with v-if="countryDetails" won't be rendered, and you won't encounter errors related to trying to access properties on null. It's a defensive programming approach to handle potential data availability issues. -->
-    <router-link to="/" class="bg-white rounded-md p-4 space-x-4 w-44 flex justify-between">Back</router-link>
-    <div v-if="countryDetails" class="grid grid-cols-3">
+    <div class="flex justify-start my-12">
+      <router-link to="/" class="bg-blue-600 text-white rounded-md px-4 py-3 space-x-2 flex items-center text-lg">
+      <Icon icon="charm:chevron-left" />
+      <span>Back</span>
+    </router-link>
+    </div>
+    <div v-if="isLoading" class="text-center mt-4">Loading...</div>
+    <div v-else-if="countryDetails" class="grid sm:grid-cols-2  gap-4">
       <div class="col-span-1">
-        <span :class="countryCode"></span>
+        <span :class="countryCode" class="text-[50vw] sm:text-[200px] md:text-[250px] rounded-lg"></span>
       </div>
-      <div class="col-span-2">
-        <p>Country name: {{ countryDetails.name.common }}</p>
+      <div class="col-span-1">
+        <div class="space-y-2 text-md">
+          <p>Official Name: <span class="font-semibold">{{ countryDetails.name.official }}</span></p>
+          <p>Languages: <span class="font-semibold">{{ Object.values(countryDetails.languages).join(', ') }}</span></p>
+          <p>Continent: <span class="font-semibold">{{ countryDetails.continents[0] }}</span></p>
+          <p>Population: <span class="font-semibold">{{ countryDetails.population.toLocaleString() }}</span></p>
+          <p>Capital: <span class="font-semibold">{{ Object.values(countryDetails.capital).join(', ') }}</span></p>
+          <p>Area: <span class="font-semibold">{{ countryDetails.area.toLocaleString() }} km<sup>2</sup></span></p>
+          <p>Currency: <span class="font-semibold">{{ Object.values(countryDetails.currencies)[0].name }}</span></p>
+        </div>
       </div>
-      
-      
-      
     </div>
   </main>
 </template>
 
 
 <script setup>
-import { useCountriesStore } from '../stores/countriesStore'
 import { ref, computed, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import { Icon } from '@iconify/vue'
 
-const countriesStore = useCountriesStore()
 const countryDetails = ref(null)
+const isLoading = ref(false)
 const route = useRoute()
 
 const fetchCountryDetails = async () => {
   const cca2 = route.params.cca2
-  countryDetails.value = countriesStore.countriesData.find(country => country.cca2 === cca2)
+  isLoading.value = true;
+  try {
+    const response = await fetch(`https://restcountries.com/v3.1/alpha/${cca2}`)
+    const data = await response.json()
+    countryDetails.value = data[0]
+    console.log(countryDetails.value)
+  } catch (error) {
+    console.error('Error fetching country details:', error)
+  } finally {
+    isLoading.value = false
+  }
 };
 
 const countryCode = computed(() => {
