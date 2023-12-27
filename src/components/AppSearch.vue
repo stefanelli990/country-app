1bea83ccb2c49b2d10f6fac92d44a1c1cec9df10
stefanelli990 <stefan.mohenski@gmail.com>
2023-12-28 00:08:38 +0100

    app done

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -4,14 +4,14 @@
     <div class="flex flex-col space-y-4 justify-between items-center my-12 sm:flex-row sm:space-y-0">
         <div class="relative w-full sm:max-w-sm md:max-w-md">
             <Icon class="text-gray-400 absolute top-1/2 left-4 -translate-y-1/2" width="24" height="24" icon="mdi:magnify" />
-            <input v-model="countriesStore.searchTerm" class="bg-white rounded-md w-full p-4 pl-12 border border-gray-200 outline-blue-600 placeholder:font-normal" type="text" placeholder="Search for a country...">
+            <input v-model="countriesStore.searchTerm" class="bg-white rounded-md w-full p-4 pl-12 border border-slate-200 outline-blue-600 placeholder:font-normal" type="text" placeholder="Search for a country...">
         </div>
-        <div class="relative w-full sm:w-44 z-10">
-            <button @click="toggleDropdown" class="p-4 bg-white rounded-md flex items-center justify-between space-x-4 w-full" ref="toggle">
+        <div class="relative w-full  sm:w-44 z-10">
+            <button @click="toggleDropdown" class="p-4 bg-white rounded-md shadow-md shadow-slate-200 flex items-center justify-between space-x-4 w-full" ref="toggle">
                 <span>{{ countriesStore.filter }}</span>
                 <Icon icon="tabler:chevron-down" />
             </button>
-            <ul v-show="dropdownIsShown" class="absolute top-12 left-0 bg-white  w-full rounded-b-md cursor-pointer overflow-hidden" ref="dropdown-content">
+            <ul v-show="dropdownIsShown" class="absolute top-14 left-0 bg-white w-full rounded-md cursor-pointer overflow-hidden shadow-md shadow-slate-200" ref="dropdown-content">
                 <li class="py-3 px-4 hover:bg-blue-50" @click="countriesStore.filter = 'All'">All</li>
                 <li class="py-3 px-4 hover:bg-blue-50" @click="countriesStore.filter = 'Africa'">Africa</li>
                 <li class="py-3 px-4 hover:bg-blue-50" @click="countriesStore.filter = 'Asia'">Asia</li>
--- src/views/CountryDetailsView.vue
+++ src/views/CountryDetailsView.vue
@@ -3,7 +3,7 @@
 <template>
   <main class="max-w-3xl">
     <div class="flex justify-start my-12">
-      <router-link to="/" class="bg-blue-600 text-white rounded-md px-4 py-3 space-x-2 flex items-center text-lg">
+      <router-link to="/" class="text-blue-600 space-x-1 flex items-center text-xl">
       <Icon icon="charm:chevron-left" />
       <span>Back</span>
     </router-link>
@@ -11,17 +11,40 @@
     <div v-if="isLoading" class="text-center mt-4">Loading...</div>
     <div v-else-if="countryDetails" class="grid sm:grid-cols-2  gap-4">
       <div class="col-span-1">
-        <span :class="countryCode" class="text-[50vw] sm:text-[200px] md:text-[250px] rounded-lg"></span>
+        <div class="pb-6 flex justify-center sm:block border-b border-gray-200 sm:pb-0 sm:border-b-0">
+          <span :class="countryCode" class="text-[50vw] sm:text-[200px] md:text-[250px] rounded-lg"></span>
+        </div>
       </div>
       <div class="col-span-1">
-        <div class="space-y-2 text-md">
-          <p>Official Name: <span class="font-semibold">{{ countryDetails.name.official }}</span></p>
-          <p>Languages: <span class="font-semibold">{{ Object.values(countryDetails.languages).join(', ') }}</span></p>
-          <p>Continent: <span class="font-semibold">{{ countryDetails.continents[0] }}</span></p>
-          <p>Population: <span class="font-semibold">{{ countryDetails.population.toLocaleString() }}</span></p>
-          <p>Capital: <span class="font-semibold">{{ Object.values(countryDetails.capital).join(', ') }}</span></p>
-          <p>Area: <span class="font-semibold">{{ countryDetails.area.toLocaleString() }} km<sup>2</sup></span></p>
-          <p>Currency: <span class="font-semibold">{{ Object.values(countryDetails.currencies)[0].name }}</span></p>
+        <div class="space-y-2 text-md sm:pl-8 sm:border-l border-gray-200">
+          <div class="flex flex-col">
+            <div class="text-gray-500">Official Name</div>
+            <div class="font-semibold">{{ countryDetails.name.official }}</div>
+          </div>
+          <div class="flex flex-col">
+            <div class="text-gray-500">Continent</div>
+            <div class="font-semibold">{{ countryDetails.continents[0] }}</div>
+          </div>
+          <div class="flex flex-col">
+            <div class="text-gray-500">Languages</div>
+            <div class="font-semibold">{{ Object.values(countryDetails.languages).join(', ') }}</div>
+          </div>
+          <div class="flex flex-col">
+            <div class="text-gray-500">Area</div>
+            <div class="font-semibold">{{ countryDetails.area.toLocaleString() }} km<sup>2</sup></div>
+          </div>
+          <div class="flex flex-col">
+            <div class="text-gray-500">Population</div>
+            <div class="font-semibold">{{ countryDetails.population.toLocaleString() }}</div>
+          </div>
+          <div class="flex flex-col">
+            <div class="text-gray-500">Capital</div>
+            <div class="font-semibold">{{ Object.values(countryDetails.capital).join(', ') }}</div>
+          </div>
+          <div class="flex flex-col">
+            <div class="text-gray-500">Currency</div>
+            <div class="font-semibold">{{ Object.values(countryDetails.currencies)[0].name }}</div>
+          </div>
         </div>
       </div>
     </div>
