b12759cef32951ae87f8896ebf55ff6a33989168
stefanelli990 <stefan.mohenski@gmail.com>
2024-02-18 23:14:59 +0100

    added appdropdown and appsearch in searchcountries view

--- src/views/SearchCountriesView.vue
+++ src/views/SearchCountriesView.vue
@@ -2,17 +2,18 @@
 
 <template>
   <main>
-    <AppSearch/>
+    <div class="flex flex-col space-y-4 justify-between items-center py-8 sticky top-0 z-10 duration-150 backdrop-blur-md bg-slate-50/80 dark:bg-slate-900/80 sm:flex-row sm:space-y-0">
+      <AppSearch/>
+      <AppDropdown/>
+    </div>
     <CountryList/>
   </main>
 </template>
 
 <script setup>
 
-
+import AppDropdown from '../components/AppDropdown.vue';
 import AppSearch from '../components/AppSearch.vue'
 import CountryList from '../components/CountryList.vue'
-
-
 
 </script>
